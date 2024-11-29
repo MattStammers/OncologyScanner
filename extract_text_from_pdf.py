1617a31fd425412d47aff6bf90125eee41cd0f5d
Hamza Afzal <100214665+snrber@users.noreply.github.com>
2024-11-29 13:24:49 +0000

    Create extract_text_from_pdf.py
    Function that allows the text of a PDF to be converted into raw text.

---
+++ extract_text_from_pdf.py
@@ -0,0 +1,12 @@
+import PyPDF2
+
+# Function to extract text from PDF
+def extract_text_from_pdf(file_path):
+    text = ""
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page.extract_text()
+    return text
