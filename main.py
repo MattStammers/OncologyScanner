4f152bc887026007be748c25a9df33fd24cc72c9
Hamza Afzal <100214665+snrber@users.noreply.github.com>
2024-12-02 22:46:35 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 # Flask app
 from flask import Flask, render_template, request, redirect, url_for
 from extract_text_from_pdf import extract_text_from_pdf 
-#from analyse_text import analyse_text
+from analyse_text import analyse_text
 
 # Set environment variables in script
 import os
-
-#from sparknlp.pretrained import PretrainedPipeline
-#from pyspark.sql import SparkSession
-
-#import sparknlp
 
 # Flask object
 app = Flask(__name__)
@@ -45,11 +40,11 @@ def upload():
         text = extract_text_from_pdf(file_path)
         
         # Analyse text with spark-nlp
-        #extracted_info = analyse_text(text)
+        extracted_info = analyse_text(text)
         
         # For now, we just display the extracted text
-        #return render_template('display_extracted_info.html', extracted_info=extracted_info)
-        return render_template('display_text.html', extracted_text=text)
+        return render_template('display_extracted_info.html', extracted_info=extracted_info)
+        #return render_template('display_text.html', extracted_text=text)
         #return f'File "{file.filename}" uploaded.'
     
     return 'Invalid file format. Please upload PDF.'
